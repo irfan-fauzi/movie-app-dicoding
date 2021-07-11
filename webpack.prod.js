fbe830b949a2195ae9a05e71132fcf720b24e2fc
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-11 13:00:20 +0700

    webpack build ok

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,17 +5,16 @@ module.exports = merge(common, {
   mode: "production",
   module: {
     rules: [
+      //babel loader
       {
         test: /\.js$/,
         exclude: "/node_modules/",
         use: [
           {
             loader: "babel-loader",
-            options: [
-              {
-                presets: ["@babel/preset-env"]
-              }
-            ]
+            options: {
+              presets: ["@babel/preset-env"]
+            }
           }
         ]
       }
