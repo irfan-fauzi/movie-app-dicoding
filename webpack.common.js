edbf469a4bed2557784c43f8a4e0f8413000a306
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-11 12:46:45 +0700

    set up webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,1 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+
+  module : {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "index.html"
+    })
+  ]
+}
